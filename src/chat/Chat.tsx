1b313da77105c423045e64aa143276652d534b2b
Bryan <bryan.lewicki.99@gmail.com>
2024-04-15 11:11:46 +0200

    test: data-testid's were added

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -28,9 +28,9 @@ export default function Chat() {
             :
             <React.Fragment>
               {
-                is?.sidebar && <div className={styles.sider}>
+                is?.sidebar && <div className={styles.sider} data-testid="ConversationSideBar">
                   <div className={styles.search}>
-                    <Search onSearch={onSearch} />
+                    <Search onSearch={onSearch} dataTestId="ConversationSearchBar"/>
                   </div>
                   <ScrollView>
                     {is?.apps ? <Apps /> : <ChatList />}
