0e77d4790b508bd9e27564acb6c344a31b72febf
Christian Gawron <gawron.christian@fh-swf.de>
2025-02-13 22:45:13 +0100

    Datenschutz (#37)
    * wip: datenschutzerklärung
    
    * chore: update Datenschutzhinweise

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve, join } from 'path'
+import * as fs from 'fs';
+
+const redirectToDir = (root = "public") => ({
+    name: 'redirect-to-dir',
+    configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+            const filePath = join(root, req.url);
+            fs.stat(filePath, (err, stats) => {
+                if (!err && stats.isDirectory() && !req.url.endsWith('/')) {
+                    res.statusCode = 302;
+                    res.setHeader('Location', req.url + "/index.html");
+                    res.setHeader('Content-Length', '0');
+                    res.end();
+                    return;
+                }
+                next();
+            });
+        })
+    },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
-    plugins: [react(),],
+    plugins: [react(), redirectToDir()],
     css: {
         preprocessorOptions: {
             less: {
@@ -14,4 +35,4 @@ export default defineConfig({
             },
         },
     }
-})
+}) 
