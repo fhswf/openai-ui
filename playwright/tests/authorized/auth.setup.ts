898da8c816eca7b6432a31764b9c951423688120
Christian Gawron <gawron.christian@fh-swf.de>
2025-11-24 23:11:06 +0000

    chore: code quality

--- playwright/tests/authorized/auth.setup.ts
+++ playwright/tests/authorized/auth.setup.ts
@@ -1,11 +1,9 @@
 import { test as setup, expect } from '@playwright/test';
-import path from 'path';
+import path from 'node:path';
 
 const authFile = path.join('playwright/.auth/user.json');
 
 setup('Cluster Login Test', async ({ page }, testInfo) => {
-    //const authFile = testInfo.outputPath('user.json');
-
 
     await page.goto('https://openai.ki.fh-swf.de');
     await expect(page.getByRole('button', { name: 'SSO Login mit der FH Kennung' })).toBeVisible();
