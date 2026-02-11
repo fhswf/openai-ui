7a61e3e855765ea77a3d56447de90d1eb2aeaa12
Bleron Beq <bleroacc@gmail.com>
2026-02-11 12:24:15 +0000

    test: add Playwright tests

--- src/chat/ChatApp.tsx
+++ src/chat/ChatApp.tsx
@@ -7,18 +7,16 @@ import { ProgressCircle } from "@chakra-ui/react";
 
 import "./style.less";
 const Chat = React.lazy(() => import("./Chat"));
-const TestPage = React.lazy(() => import("../TestPage"));
 
 export default function ChatApp() {
   const loading = <ProgressCircle.Root size="lg" color="blue.400" />;
-  const isTest = new URLSearchParams(window.location.search).get("test") === "true";
 
   return (
     <Suspense fallback={loading}>
       <ErrorBoundary fallbackRender={ErrorFallback}>
         <ChatProvider>
           <AppsProvider>
-            {isTest ? <TestPage /> : <Chat />}
+            <Chat />
           </AppsProvider>
         </ChatProvider>
       </ErrorBoundary>
